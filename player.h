62bf8159839bb6f86ea7fa52c8c14ff2aa8b1b03
AgostinhoTam <agostinhotam@gmail.com>
2023-10-29 14:28:02 +0900

    プレイヤークラス_v1

--- player.h
+++ player.h
@@ -18,11 +18,22 @@
 //==================================
 class Player :public Obj
 {
+private:
+	float	m_faith = 0; //M‹Â’l
+	int		m_combo = 0;
+	float	m_hp = 0;
 public:
 	Player();
 	~Player();
 	void Update(void)override;
 	void Draw(void)override;
+	float	GetFaith() { return m_faith; }
+	int		GetCombo() { return m_combo; }
+	float	GetHp() { return m_hp; }
+	void	AddHp(float addhp) { m_hp += addhp; }
+	void	AddFaith(float addfaith) { m_faith += addfaith; }
+	void	AddCombo(float addcombo) { m_combo += addcombo; }
+	void	ComboReset() { m_combo = 0; }
 };
 
 
